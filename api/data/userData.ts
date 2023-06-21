9c611c03ff130a739d57d0b57262a18110f5fc56
k-siepka <k.siepka97@gmail.com>
2023-06-21 09:38:40 +0100

    test: logging user happy path

--- api/data/userData.ts
+++ api/data/userData.ts
@@ -1,52 +1,81 @@
 export interface UserParam {
   firstName?: string;
-  lastName?: string ;
+  lastName?: string;
   email?: string;
   password?: string;
   description?: string;
-  expectedMessage: string;
+  expectedMessage?: string;
 }
 
 export default class UserData {
   getUsers(): UserParam[] {
     return [
       {
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        description: 'empty data',
-        expectedMessage: 'Email address is already in use'
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        description: "empty data",
+        expectedMessage: "Email address is already in use",
       },
       {
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'wrongEmail',
-        password: 'password123',
-        description: 'incorrect email address',
-        expectedMessage: 'Email address is already in use'
+        firstName: "firstName",
+        lastName: "lastName",
+        email: "wrongEmail",
+        password: "password123",
+        description: "incorrect email address",
+        expectedMessage: "Email address is already in use",
       },
       {
-        firstName: 'firstName',
-        lastName: 'lastName',
-        email: 'test@test.wp',
-        password: 'pass',
-        description: 'password too short',
-        expectedMessage: 'Email address is already in use'
+        firstName: "firstName",
+        lastName: "lastName",
+        email: "test@test.wp",
+        password: "pass",
+        description: "password too short",
+        expectedMessage: "Email address is already in use",
       },
-    ]
+    ];
   }
 
-  getCreateUserData():  UserParam[] {
+  getCreateUserData(): UserParam[] {
     return [
       {
-        firstName: 'userToBeCreated',
-        lastName: 'userToBeCreated',
-        email: 'test333@test.wp',
-        password: 'passPASS',
-        description: 'password too short',
-        expectedMessage: 'Email address is already in use'
+        firstName: "userToBeCreated",
+        lastName: "userToBeCreated",
+        email: "test333@test.wp",
+        password: "passPASS",
+        description: "",
+        expectedMessage: "",
       },
-    ]
+    ];
+  }
+  getLoginUser(): UserParam[] {
+    return [
+      {
+        email: "test333@test.wp",
+        password: "wrongPassword",
+        description: "Invalid Password",
+      },
+      {
+        email: "invalidEmail",
+        password: "passPASS",
+        description: "Invalid Email",
+      },
+      {
+        email: "",
+        password: "passPass",
+        description: "No email",
+      },
+      {
+        email: "test333@test.wp",
+        password: "",
+        description: "No password",
+      },
+      {
+        email: "",
+        password: "",
+        description: "No credentials",
+      },
+    ];
   }
 }
--- api/requests/usersRequester.ts
+++ api/requests/usersRequester.ts
@@ -3,32 +3,72 @@ import BasicRequester from "./basicRequester";
 
 export default class UsersRequester extends BasicRequester {
   async addUser(testCase: UserParam) {
-    const {firstName, lastName, email, password, description, expectedMessage} = testCase;
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      description,
+      expectedMessage,
+    } = testCase;
     const body = {
-      "firstName": firstName,
-      "lastName": lastName,
-      "email": email,
-      "password": password
-    }
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    };
 
-    return await this.client.post('users').send(body);
+    return await this.client.post("users").send(body);
   }
 
   async loginUser(testCase: UserParam) {
-    const {firstName, lastName, email, password, description, expectedMessage} = testCase;
-    let  body = {
-      "email": email,
-      "password": password
-    }
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      description,
+      expectedMessage,
+    } = testCase;
+    let body = {
+      email: email,
+      password: password,
+    };
 
-    return await this.client.post('users/login').send(body);
+    return await this.client.post("users/login").send(body);
   }
 
-  async deleteUser(token: string){
+  async deleteUser(token: string) {
     const header = {
-      'Authorization': `Bearer ${token}`,
-    }
+      Authorization: `Bearer ${token}`,
+    };
 
-    return await this.client.delete('users/me').set(header);
+    return await this.client.delete("users/me").set(header);
+  }
+  async loginUserNoPassword(testCase: UserParam) {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      description,
+      expectedMessage,
+    } = testCase;
+    let body = {
+      email: email,
+      password: password,
+    };
+
+    return await this.client.post("users/login").send(body);
+  }
+
+  async loginUserWithInvalidPassword(testCase: UserParam) {
+    const { email, password } = testCase;
+    let body = {
+      email: email,
+      password: password,
+    };
+
+    return await this.client.post("users/login").send(body);
   }
 }
